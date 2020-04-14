9d5f07e1c5de25c89a1243f9b2e2f2c802508088
SidWadhera <siwadhera@packer.edu>
2020-04-13 21:09:23 -0400

    Changing Texts, finishing up

--- script.js
+++ script.js
@@ -19,6 +19,17 @@ function getWords() {
   document.getElementById("word6").innerHTML = document.getElementById("word6Entry").value;
   document.getElementById("word7").innerHTML = document.getElementById("word7Entry").value;
   document.getElementById("word8").innerHTML = document.getElementById("word8Entry").value;
+  document.getElementById("word9").innerHTML = document.getElementById("word9Entry").value;
+  document.getElementById("word10").innerHTML = document.getElementById("word10Entry").value;
+  document.getElementById("word11").innerHTML = document.getElementById("word11Entry").value;
+  document.getElementById("word12").innerHTML = document.getElementById("word12Entry").value;
+  document.getElementById("word13").innerHTML = document.getElementById("word13Entry").value;
+  document.getElementById("word14").innerHTML = document.getElementById("word14Entry").value;
+  document.getElementById("word15").innerHTML = document.getElementById("word15Entry").value;
+  document.getElementById("word16").innerHTML = document.getElementById("word16Entry").value;
+  document.getElementById("word17").innerHTML = document.getElementById("word17Entry").value;
+  document.getElementById("word18").innerHTML = document.getElementById("word18Entry").value;
+
 
   document.getElementById("results").style.display = "block";
 }
